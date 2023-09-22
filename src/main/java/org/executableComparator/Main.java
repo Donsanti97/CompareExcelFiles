7c2b0ae17ab54482d2c966ecb91283033568608f
Mairon Santiago Martinez Rojas <Msmartinez880@misena.edu.co>
2023-09-22 09:14:38 -0500

    Clase main que llama la clase Start

---
+++ src/main/java/org/executableComparator/Main.java
@@ -0,0 +1,14 @@
+package org.executableComparator;
+
+import java.io.IOException;
+
+import org.utils.Start;
+
+import static org.utils.MethotsAzureMasterFiles.getDocument;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Start start = new Start();
+        start.start();
+    }
+}
